0749137a819ba4a8b8672602366be12d2d3056ed
D2Hydro <daniel@d2hydro.nl>
2025-09-18 21:03:54 +0200

    improved twinpy conversion

--- src/fewspy/__init__.py
+++ src/fewspy/__init__.py
@@ -3,5 +3,6 @@ from fewspy.io.read_xml import read_xml
 from fewspy.io.read_json import read_json
 from fewspy.io.read_netcdf import read_netcdf
 from fewspy.io.read_parquet import read_parquet
+from fewspy.io.write_netcdf import write_netcdf
 
-__all__ = ["Api", "read_xml", "read_json", "read_netcdf", "read_parquet"]
+__all__ = ["Api", "read_xml", "read_json", "read_netcdf", "read_parquet", "write_netcdf"]
--- tests/hhnk/convert_twinpy.py
+++ tests/hhnk/convert_twinpy.py
@@ -1,9 +1,8 @@
 #%%
 from pathlib import Path
-import pandas as pd
 import pickle
 import os
-from fewspy.time_series import TimeSeries, TimeSeriesSet, Header, Events
+
 from TWINpy.app.TWIN_get_timeseries import TWIN_Timeserie
 
 
@@ -16,7 +15,22 @@ with open(data_dir / "twin_response.pickle", "rb") as src:
 
 
 #%%
+from fewspy.time_series import TimeSeries, TimeSeriesSet, Header, Events
+
 def convert_timeseries(time_series:TWIN_Timeserie, header_info: dict = {"type": "instantaneous", "time_step": {"unit": "nonequidistant"}}) -> TimeSeries:
+    """Converts TWIN_Timeserie to fewspy.TimeSeriesSet
+
+    Parameters
+    ----------
+    time_series : TWIN_Timeserie
+    header_info : dict, optional
+        Header-info missing in TWIN_Timeserie, by default {"type": "instantaneous", "time_step": {"unit": "nonequidistant"}}
+
+    Returns
+    -------
+    TimeSeries
+        _description_
+    """
     header = {**time_series.ts_header, **header_info}
     header["miss_val"] = header.pop("missing_val")
     header["start_date"] = time_series.df.index.min()
@@ -25,11 +39,5 @@ def convert_timeseries(time_series:TWIN_Timeserie, header_info: dict = {"type": 
     return TimeSeries(header=Header(**header), events=Events(time_series.df))
  
 time_series_set = TimeSeriesSet(time_series = [convert_timeseries(i) for i in b.values()])
-
-
-with open(data_dir / "twin_fewspy_ts.pickle", 'wb') as handle:
-    pickle.dump(time_series_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-
+time_series_set.to_netcdf(out_dir=Path("netcdf"))
 # %%
